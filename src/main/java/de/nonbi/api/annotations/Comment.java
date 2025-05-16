c271e4be3354904b848dd8f02d61c816c317cb58
Nyx <nyx@veero.de>
2025-05-16 21:24:06 +0200

    update and adding annotations

---
+++ src/main/java/de/nonbi/api/annotations/Comment.java
@@ -0,0 +1,13 @@
+package de.nonbi.api.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Comment {
+    String author() default "...";
+    String msg() default "...";
+}
