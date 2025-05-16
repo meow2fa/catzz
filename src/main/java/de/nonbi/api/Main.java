50673ef2498d1946e1435dd3f92ed632d9bfbe34
Nyx <nyx@veero.de>
2025-05-16 21:24:30 +0200

    update Main and Gradle Script

--- src/main/java/de/nonbi/api/Main.java
+++ src/main/java/de/nonbi/api/Main.java
@@ -3,23 +3,13 @@ package de.nonbi.api;
 import de.nonbi.api.route.RouteManager;
 import io.javalin.Javalin;
 
-import java.util.Scanner;
-
 public class Main {
 
     private static Javalin app;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\nPort: ");
-        int port;
-        if (scanner.nextLine().isEmpty()) {
-            port = 7070;
-        }else {
-            port = Integer.valueOf(scanner.nextLine());
-        }
-        app = Javalin.create().start(port);
-        RouteManager.registerAll(app);
+        app = Javalin.create().start(7070);
+        RouteManager.registerRoutes(app, "de.nonbi.api.controller");
     }
 
     public static Javalin getApp() {
