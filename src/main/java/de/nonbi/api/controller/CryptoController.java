a34ea82d7fd8a03e038c9da334281c195c785ab2
Nyx <nyx@veero.de>
2025-05-16 21:24:52 +0200

    update CryptoController for Route Registration

--- src/main/java/de/nonbi/api/controller/CryptoController.java
+++ src/main/java/de/nonbi/api/controller/CryptoController.java
@@ -1,17 +1,23 @@
 package de.nonbi.api.controller;
 
+import de.nonbi.api.annotations.Comment;
+import de.nonbi.api.annotations.Route;
+import de.nonbi.api.annotations.RouteHandler;
 import de.nonbi.api.utils.Crypto;
 import io.javalin.http.Context;
 
 import java.util.Map;
 
-public class CryptoController {
+@Comment(author = "Niko", msg = "Simple De/-Encrypt Controller")
+public class CryptoController implements RouteHandler {
+
+    @Route(r = "/api/text/encode", method = "GET")
     public static void encode(Context ctx) {
         String text = ctx.queryParam("text");
         String key = ctx.queryParam("key");
 
         if (text == null || key == null) {
-            ctx.status(400).json(Map.of("error", "Fehlender text oder key"));
+            ctx.status(400).json(Map.of("error", "Missing Key or Text"));
             return;
         }
 
@@ -19,12 +25,13 @@ public class CryptoController {
         ctx.json(Map.of("result", result));
     }
 
+    @Route(r = "/api/text/decode", method = "GET")
     public static void decode(Context ctx) {
         String text = ctx.queryParam("text");
         String key = ctx.queryParam("key");
 
         if (text == null || key == null) {
-            ctx.status(400).json(Map.of("error", "Fehlender text oder key"));
+            ctx.status(400).json(Map.of("error", "Missing Key or Text"));
             return;
         }
 
